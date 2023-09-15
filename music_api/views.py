180aa6f9b9c8b7a3a8d3ccb35c7aee2c6ad7182b
Collins Kasyoki <collinskasyoki@gmail.com>
2023-09-15 19:42:43 +0300

    Add artists path

---
+++ music_api/views.py
@@ -0,0 +1,16 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import ArtistSerializer, AlbumSerializer, SongSerializer
+
+from .models import Artist, Album, Song
+
+class ArtistViewSet(APIView):
+
+    def get(self, request, *args, **kwargs):
+        # Filter options?
+
+        queryset = Artist.objects.all()
+        serializer = ArtistSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
