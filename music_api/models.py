06b52c0ffd7e1313cc88d5db3bcbbfbb394d0be2
Collins Kasyoki <collinskasyoki@gmail.com>
2023-09-15 13:52:20 +0300

    Update models

--- music_api/models.py
+++ music_api/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
-class Artist:
-    name = models.CharField(200)
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
 
 
-class Album:
-    name = models.CharField(200)
-    albumartist = models.ForeignKey(Artist, on_delete=models.RESTRICT, on_update=models.CASCADE)
+class Album(models.Model):
+    name = models.CharField(max_length=200)
+    albumartist = models.ForeignKey(Artist, on_delete=models.RESTRICT)
 
 
-class Song:
-    title = models.CharField(300)
-    album = models.ForeignKey(Album, on_delete=models.RESTRICT, on_update=models.CASCADE)
-    artist = models.ForeignKey(Artist, on_delete=models.RESTRICT, on_update=models.CASCADE, null=True)
+class Song(models.Model):
+    title = models.CharField(max_length=300)
+    album = models.ForeignKey(Album, on_delete=models.RESTRICT)
+    artist = models.ForeignKey(Artist, on_delete=models.RESTRICT)
     length = models.IntegerField()
     year = models.SmallIntegerField()
-    genre = models.CharField(100)
+    genre = models.CharField(max_length=100)
     lyrics = models.TextField(null=True)
