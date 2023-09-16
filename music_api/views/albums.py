c23d5f487bfccd73b0c8d03edf5688464c1d7bac
Collins Kasyoki <collinskasyoki@gmail.com>
2023-09-16 13:09:30 +0300

    Add genre filter on albums endpoint

--- music_api/views/albums.py
+++ music_api/views/albums.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404
 
+from rest_framework import status
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from drf_yasg.utils import swagger_auto_schema
@@ -13,13 +15,24 @@ from music_api.models import Album
 class AlbumListSet(APIView):
 
     @swagger_auto_schema(
-            operation_summary="List all albums",
+            operation_summary="List albums",
+            manual_parameters=[
+                openapi.Parameter(
+                    name='genre',
+                    in_=openapi.IN_QUERY,
+                    type=openapi.TYPE_STRING,
+                    description='Filter by genre name',
+                )
+            ],
             responses={200: openapi.Response("List of albums", AlbumSerializer(many=True))}
     )
     def get(self, request, *args, **kwargs):
-        # Filter options?
+        genre_filter = request.query_params.get('genre')
 
         queryset = Album.objects.all()
+        if genre_filter:
+            queryset = queryset.filter(genre=genre_filter)
+
         serializer = AlbumSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
